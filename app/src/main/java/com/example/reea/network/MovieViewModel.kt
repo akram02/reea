251c3738adb2f1780c7f3d013da5c41fad0413e4
Akram Khan <akrammiru@gmail.com>
2022-05-28 22:57:54 +0600

    added data binding in movie details

--- app/src/main/java/com/example/reea/network/MovieViewModel.kt
+++ app/src/main/java/com/example/reea/network/MovieViewModel.kt
@@ -2,7 +2,7 @@ package com.example.reea.network
 
 import androidx.lifecycle.MutableLiveData
 import com.example.reea.base.BaseViewModel
-import com.example.reea.network.RestApi
+import com.example.reea.vm.MovieDetailsVM
 import com.example.reea.vm.MovieListVM
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -11,6 +11,7 @@ import javax.inject.Inject
 class MovieViewModel @Inject constructor(private val api: RestApi): BaseViewModel() {
 
     val movieListLiveData = MutableLiveData<MovieListVM>()
+    val movieDetails = MutableLiveData<MovieDetailsVM>()
 
     fun getMovieList() {
         getResponse(api::getMovieList) {
@@ -20,7 +21,7 @@ class MovieViewModel @Inject constructor(private val api: RestApi): BaseViewMode
 
     fun getMovieDetails(id: Int) {
         getResponse(api::getMovieDetails, id) {
-
+            movieDetails.value = it
         }
     }
 }
--- app/src/main/java/com/example/reea/ui/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/reea/ui/MovieDetailsFragment.kt
@@ -5,14 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.reea.databinding.FragmentMovieDetailsBinding
+import com.example.reea.network.MovieViewModel
 import com.example.reea.vm.MovieVM
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MovieDetailsFragment : Fragment() {
 
-    lateinit var binding: FragmentMovieDetailsBinding
+    private lateinit var binding: FragmentMovieDetailsBinding
+
+    private val viewModel by viewModels<MovieViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,8 +24,10 @@ class MovieDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMovieDetailsBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
         val movie: MovieVM = MovieDetailsFragmentArgs.fromBundle(requireArguments()).movie
-        binding.title.text = movie.title
+        viewModel.getMovieDetails(movie.id ?: 0)
         return binding.root
     }
 }
